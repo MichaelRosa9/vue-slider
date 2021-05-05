f6c71570f7bb24bdafef2ee09ecb93c17560dac5
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-05 16:27:00 +0200

    indicators

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    counterPhoto: 0,
+    arrayPhotos:['img/city-1.jpg','img/city-2.jpg','img/city-3.jpg','img/city-4.jpg','img/city-5.jpg',]
   },
   methods: {
+    nextPhoto: function(){
+      this.counterPhoto++;
+      if (this.counterPhoto === this.arrayPhotos.length){
+        this.counterPhoto = 0;
+      }
+    },
+    previousPhoto: function(){
+      this.counterPhoto--;
+      if (this.counterPhoto < 0){
+        this.counterPhoto = this.arrayPhotos.length -1;
+      }
+    }
     
   }
-})
+});
